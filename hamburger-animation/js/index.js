eb30c4262c7d83c6913f52e9a86ebac638b09dff
Valeriy Alikin <valeriy@da-group.ru>
2018-03-19 15:26:59 +0500

    remove projects

--- hamburger-animation/js/index.js
+++
@@ -1,9 +0,0 @@
-$('.menu').click(function() {
-	if($(this).is('.active:not(.back)')) {
-		 $(this).addClass('back');
-	} else if ($(this).is('.back')) {
-		$(this).removeClass('back');
-	} else {
-		$(this).addClass('active');
-	}
-});
