843df218c80ee46ba30f16b9dba16b24fbab40f4
Valeriy Alikin <valeriy@da-group.ru>
2018-03-19 10:51:39 +0500

    fix map

--- gorod/js/main.js
+++ gorod/js/main.js
@@ -131,6 +131,20 @@ $(document).ready(function(){
       $('.header__menu').toggleClass('showMenu');
     });
 
+    var uluru = {lat: 58.007606, lng: 56.249746};
+    var map = new google.maps.Map(document.getElementById('fullMap'), {
+      zoom: 17,
+      center: uluru,
+      styles: [{"stylers":[{"saturation":-100},{"gamma":1}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"saturation":50},{"gamma":0},{"hue":"#50a5d1"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#333333"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"weight":0.5},{"color":"#333333"}]},{"featureType":"transit.station","elementType":"labels.icon","stylers":[{"gamma":1},{"saturation":50}]}]
+    });
+
+    var image = 'img/marker.png';
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+      icon: image
+    });
+
 });
 
 $( window ).load(function() {
@@ -156,19 +170,3 @@ $(window).on('resize', function(){
       $('.news-main .card__info').css('height','auto');
     }     
 });
-
-function initMap() {
-  var uluru = {lat: 58.007606, lng: 56.249746};
-  var map = new google.maps.Map(document.getElementById('fullMap'), {
-    zoom: 17,
-    center: uluru,
-    styles: [{"stylers":[{"saturation":-100},{"gamma":1}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"saturation":50},{"gamma":0},{"hue":"#50a5d1"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#333333"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"weight":0.5},{"color":"#333333"}]},{"featureType":"transit.station","elementType":"labels.icon","stylers":[{"gamma":1},{"saturation":50}]}]
-  });
-
-  var image = 'img/marker.png';
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-    icon: image
-  });
-}
