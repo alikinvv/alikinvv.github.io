69e0ec7748fd47d5f1ea4ca13d446c96a90d967f
Valery Alikin <valeriy@da-group.ru>
2018-03-19 10:06:03 +0500

    fix google map

--- gorod/js/main.js
+++ gorod/js/main.js
@@ -131,6 +131,29 @@ $(document).ready(function(){
       $('.header__menu').toggleClass('showMenu');
     });
 
+    google.maps.event.addDomListener(window, 'load', init);
+
+    function init() {
+       var mapOptions = {
+            zoom: 17,
+            center: new google.maps.LatLng(58.007606, 56.249746),
+
+           styles: [{"stylers":[{"saturation":-100},{"gamma":1}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"saturation":50},{"gamma":0},{"hue":"#50a5d1"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#333333"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"weight":0.5},{"color":"#333333"}]},{"featureType":"transit.station","elementType":"labels.icon","stylers":[{"gamma":1},{"saturation":50}]}]
+        };
+
+        var mapElement = document.getElementById('fullMap');
+
+        var map = new google.maps.Map(mapElement, mapOptions);
+
+      var image = 'img/marker.png';
+
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(58.007606, 56.249746),
+            map: map,
+            icon: image
+        });
+    }
+
 });
 
 $( window ).load(function() {
@@ -156,29 +179,3 @@ $(window).on('resize', function(){
       $('.news-main .card__info').css('height','auto');
     }     
 });
-
-if($('*').hasClass('mapWrap')) {
-  google.maps.event.addDomListener(window, 'load', init);
-
-  function init() {
-     var mapOptions = {
-          zoom: 17,
-          center: new google.maps.LatLng(58.007606, 56.249746),
-
-         styles: [{"stylers":[{"saturation":-100},{"gamma":1}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"saturation":50},{"gamma":0},{"hue":"#50a5d1"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#333333"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"weight":0.5},{"color":"#333333"}]},{"featureType":"transit.station","elementType":"labels.icon","stylers":[{"gamma":1},{"saturation":50}]}]
-      };
-
-      var mapElement = document.getElementById('fullMap');
-
-      var map = new google.maps.Map(mapElement, mapOptions);
-
-    var image = 'img/marker.png';
-
-      var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(58.007606, 56.249746),
-          map: map,
-          icon: image
-      });
-  }
-
-}
